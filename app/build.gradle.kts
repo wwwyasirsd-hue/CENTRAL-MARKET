653cbc2220aa00e19a4bd02b71611ec031d55cd5
wwwyasirsd-hue <www.yasir.sd@gmail.com>
2026-09-22 01:23:35 +0200

    إضافة إعدادات تطبيق Android الأساسية

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.central.market"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.central.market"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
